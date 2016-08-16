503299d56f9e761416d3994e64abe333ed599c5c
Julian Gonggrijp <j.gonggrijp@gmail.com>
2016-08-17 00:58:45 +0200

    Fix incorrect Jasmine matcher

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ describe('this spec', function() {
 	beforeEach(module('uiGmapgoogle-maps'));
 	
 	it('tries to configure uiGmapGoogleMapApiProvider', inject(function(uiGmapGoogleMapApiProvider) {
-		expect(uiGmapGoogleMapApiProvider.configure).toExist();
+		expect(uiGmapGoogleMapApiProvider.configure).toBeDefined();
 	}));
 });
